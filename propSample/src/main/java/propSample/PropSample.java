7d9ff133d20a22362547411521987aeb5d6c20a7
yuki-kikuya <yuki-kikuya@SCC41K170340802>
2017-08-21 14:30:49 +0900

    add

--- propSample/src/main/java/propSample/PropSample.java
+++ propSample/src/main/java/propSample/PropSample.java
@@ -3,22 +3,29 @@ package propSample;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class PropSample {
     public static void main(String[] args) {
         FileInputStream fis;
+        Properties props = new Properties();
         try {
-            fis = new FileInputStream(new File("wps.conf"));
+            fis = new FileInputStream(new File("wps.conf"));   
+            props.load(fis); // 読み込み
+            fis.close();
         } catch (FileNotFoundException e) {
+            // TODO 自動生成された catch ブロック
+            e.printStackTrace();
+        } catch (IOException e) {
             // TODO 自動生成された catch ブロック
             e.printStackTrace();
         }
         
-        Properties props = new Properties();
-        props.load(fis); // 読み込み
-        fis.close();
-        System.out.println(props.getProperty("name"));
+        System.out.println(props.getProperty("COMMON_PAGE_PATH"));
+        System.out.println(props.getProperty("PAGE_PATH"));
+        System.out.println(props.getProperty("PATTERN"));
+
         
         
     }
